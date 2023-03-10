72ffa0aeeaf462e3f1ff67ca95b28334c4c61deb
Phil Tsaryk <phil@tsaryk.com>
2023-03-10 01:54:15 +0100

    feat(i18n): update russian translations

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ APP_CSS=app-f0a1df946d.css
 
 # scp "${SCRIPT_DIR}/../www4/scripts/app.js" volumio:/volumio/http/www4/scripts/${APP_JS}
 # scp "${SCRIPT_DIR}/../www4/styles/${APP_CSS}" volumio:/volumio/http/www4/styles/
+# scp "${SCRIPT_DIR}/../www4/app/i18n/locale-ru.json" volumio:/volumio/http/www4/app/i18n/
 
 # scp "${SCRIPT_DIR}/../www4/app/themes/volumio/assets/variants/volumio/volumio-settings.json" \
 #   volumio:/volumio/http/www4/app/themes/volumio/assets/variants/volumio/
