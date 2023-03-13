c2151d9ffc8ed19052ae1ea7f9b8926be6fcc452
Phil Tsaryk <phil@tsaryk.com>
2023-03-13 15:22:19 +0100

    chore: 3.435

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -7,3 +7,5 @@ scp -r volumio:/volumio/http/www4/app/ "${SCRIPT_DIR}/../www4/"
 scp -r volumio:/volumio/http/www4/fonts/ "${SCRIPT_DIR}/../www4/"
 scp -r volumio:/volumio/http/www4/styles/ "${SCRIPT_DIR}/../www4/"
 scp -r volumio:/volumio/http/www4/scripts/ "${SCRIPT_DIR}/../www4/"
+
+scp volumio:/volumio/http/www4/scripts/app-1e1b551a2a.js "${SCRIPT_DIR}/../www4/scripts/app.js"
