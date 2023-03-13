20990db32b45211bbbdde9b7d68226bf2e8e2d5a
Phil Tsaryk <phil@tsaryk.com>
2023-03-13 20:38:00 +0100

    chore: deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+APP_JS=app-1e1b551a2a.js
+APP_CSS=app-f0a1df946d.css
+
+# scp "${SCRIPT_DIR}/../www4/scripts/app.js" volumio:/volumio/http/www4/scripts/${APP_JS}
+# scp "${SCRIPT_DIR}/../www4/styles/${APP_CSS}" volumio:/volumio/http/www4/styles/
+
+# scp "${SCRIPT_DIR}/../www4/app/themes/volumio/assets/variants/volumio/volumio-settings.json" \
+#   volumio:/volumio/http/www4/app/themes/volumio/assets/variants/volumio/
